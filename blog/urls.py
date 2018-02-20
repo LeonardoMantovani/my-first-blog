590fd2bff7af8faa466bc0283564af6384f3abd5
Leonardo <leonardo@mantovani.us>
2018-02-20 22:22:42 +0100

    Aggiunto un file HTML di base rendendo tutti gli altri template sue estensioni. Aggiunta una pagina specifica per ogni post creato

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list') #SE NON VIENE AGGIUNTO NESSUN URL ALL'INDIRIZZO PRINCIPALE MOSTRA LA VISTA post_list
+    url(r'^$', views.post_list, name='post_list'), #SE NON VIENE AGGIUNTO NESSUN URL ALL'INDIRIZZO PRINCIPALE MOSTRA LA VISTA post_list
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'), #CREA UNA URL PER OGNI POST CHE CONSISTE IN indirizzodelsito.com/post/<post_id> CON AL POSTO DI <post_id> UN NUMERO ASSEGNATO AL POST
 ]
