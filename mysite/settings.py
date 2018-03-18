6555cd71038d002616e17f89925e126e0d5abc41
Leonardo Mantovani <leonardo@mantovani.us>
2018-03-18 15:34:35 +0100

    Spostato il database da Sqlite3 a PostgreSQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -72,13 +72,20 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
+# Database (cambiato da quello di defoult (commentato) a PostgreSQL)
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'djangotutorial',
+        'USER': 'postgres',
+        'PASSWORD': 'leosilmi192820',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
